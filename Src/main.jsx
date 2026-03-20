ddc895fd620405b7f4874c115c41542baee9a835
jschroeder71 <jschroeder71@gmail.com>
2026-03-19 20:35:01 -0400

    Import LiveMap component in main.jsx

--- Src/main.jsx
+++ Src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
+import LiveMap from './LiveMap';
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
